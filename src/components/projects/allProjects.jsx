62f434d4ce5846c7de970d8ed671c82c31c56383
Walstan Baptista <38958184+walstanb@users.noreply.github.com>
2023-07-30 00:39:27 -0700

    feat: Added Carousel ✨

--- src/components/projects/allProjects.jsx
+++ src/components/projects/allProjects.jsx
@@ -3,26 +3,53 @@ import React from "react";
 import Project from "./project";
 
 import INFO from "../../data/user";
-
+import { Carousel } from "react-responsive-carousel";
+import "../../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css";
 import "./styles/allProjects.css";
 
 const AllProjects = () => {
 	return (
-		<div className="all-projects-container">
-			{INFO.projects.project_list.map((project, index) => (
-				<div className="all-projects-project" key={index}>
-					<Project
-						title={project.title}
-						description={project.description}
-						linkText={project.linkText}
-						link={project.link}
-						image={project.image}
-						image_alt={project.image_alt}
-						skills={project.skills}
-					/>
-				</div>
-			))}
-		</div>
+		<React.Fragment>
+			<div className="all-projects-container">
+				{INFO.projects.project_list.map((project, index) => (
+					<div className="all-projects-project" key={index}>
+						<Project
+							title={project.title}
+							description={project.description}
+							linkText={project.linkText}
+							link={project.link}
+							image={project.image}
+							image_alt={project.image_alt}
+							skills={project.skills}
+						/>
+					</div>
+				))}
+			</div>
+			<Carousel
+				showThumbs={false}
+				showArrows={false}
+				autoPlay
+				swipeScrollTolerance={30}
+				preventMovementUntilSwipeScrollTolerance
+				infiniteLoop
+				stopOnHover
+				className="all-projects-carousel"
+			>
+				{INFO.projects.project_list.map((project, index) => (
+					<div className="all-projects-project" key={index}>
+						<Project
+							title={project.title}
+							description={project.description}
+							linkText={project.linkText}
+							link={project.link}
+							image={project.image}
+							image_alt={project.image_alt}
+							skills={project.skills}
+						/>
+					</div>
+				))}
+			</Carousel>
+		</React.Fragment>
 	);
 };
 
