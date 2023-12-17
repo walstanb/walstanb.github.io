32d68c26a2d37dcb5cc21f6cb4f6c0f659728e36
Walstan Baptista <38958184+walstanb@users.noreply.github.com>
2023-12-16 22:13:00 -0600

    feat: sfx on darkModeSwitch

--- src/components/common/darkModeSwitch.jsx
+++ src/components/common/darkModeSwitch.jsx
@@ -1,21 +1,71 @@
 import "./styles/darkModeSwitch.css";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useMemo } from "react";
 import { ThemeContext } from "../../ThemeContext";
 import { trackAction } from "./analytics";
 
 const DarkModeSwitch = () => {
 	const { isDarkMode, setIsDarkMode } = useContext(ThemeContext);
-
+	const darkModeSwitchRef = useRef(null);
+	const audioUp = useMemo(() => {
+		return new Audio("/sfx/up.wav");
+	}, []);
+	const audioDown = useMemo(() => {
+		return new Audio("/sfx/down.wav");
+	}, []);
 	// Function to toggle dark mode
 	const toggleDarkMode = () => {
 		setIsDarkMode((prevDarkMode) => !prevDarkMode);
 	};
+
+	useEffect(() => {
+		const preloadAudio = () => {
+			audioUp.load();
+			audioDown.load();
+		};
+		preloadAudio();
+	}, [audioDown, audioUp]);
+
+	useEffect(() => {
+		const handleMouseUp = (event) => {
+			if (
+				darkModeSwitchRef.current &&
+				!darkModeSwitchRef.current.contains(event.target)
+			)
+				return;
+			(isDarkMode ? audioDown : audioUp).play();
+		};
+
+		document.addEventListener("mouseup", handleMouseUp);
+
+		return () => {
+			document.removeEventListener("mouseup", handleMouseUp);
+		};
+	}, [isDarkMode, audioDown, audioUp]);
+
+	useEffect(() => {
+		const handleMouseDown = (event) => {
+			if (
+				darkModeSwitchRef.current &&
+				!darkModeSwitchRef.current.contains(event.target)
+			)
+				return;
+
+			(isDarkMode ? audioUp : audioDown).play();
+		};
+
+		document.addEventListener("mousedown", handleMouseDown);
+
+		return () => {
+			document.removeEventListener("mousedown", handleMouseDown);
+		};
+	}, [isDarkMode, audioDown, audioUp]);
 
 	return (
 		<div
 			className={`dark-mode-switch ${
 				isDarkMode ? "dark-mode" : "light-mode"
 			}`}
+			ref={darkModeSwitchRef}
 		>
 			<input
 				type="checkbox"
